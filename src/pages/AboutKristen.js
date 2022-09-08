03e1598d6a053d412609fe0cd4b24a1bf5d96809
Kristen Wegner <klwegner@gmail.com>
2022-09-07 21:19:53 -0400

    changed verbiage in about pages

--- src/pages/AboutKristen.js
+++ src/pages/AboutKristen.js
@@ -2,7 +2,7 @@ function AboutKristen() {
     return(
         <div className="container">
   <h1 class="display-2">About Kristen Wegner</h1>
-<p>Kristen is a full-stack web developer who loves nature, puppers and doggos, and reading.</p>
+<p>Kristen is a full-stack web developer who loves nature, puppers and doggos, reading, and so much more.</p>
 <p>When not trying hard to get React components to work, she is trying hard to get students in the k-12 public education system to work.</p>
 <p>Like most people, one of Kristen's "likes" is music; here you can hear a couple examples of beloved songs.</p></div>
 
